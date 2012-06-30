e6d2d153bec30965fd16d7603666fe48b103f3f8
jankuca <jan@jankuca.com>
2012-06-30 17:20:42 +0200

    add ObjectId#toJSON

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ mongodb.ReplicaSet = require('./src/ReplicaSet');
 mongodb.Server = require('./src/Server');
 
 mongodb.ObjectId = require('buffalo').ObjectId;
+mongodb.ObjectId.prototype.toJSON = mongodb.ObjectId.prototype.toString;
 
 module.exports = mongodb;
