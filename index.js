07d2354f9a0964aedc314eb1a73fdb5147837e3d
jankuca <jan@jankuca.com>
2014-03-25 02:47:49 +0100

    bump version number to 1.2.0

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var mongodb = {};
 mongodb.ReplicaSet = require('./src/ReplicaSet');
 mongodb.Server = require('./src/Server');
 
+mongodb.Collection = require('./src/Collection');
+mongodb.Database = require('./src/Database');
+
 mongodb.ObjectId = require('buffalo').ObjectId;
 mongodb.ObjectId.prototype.toJSON = mongodb.ObjectId.prototype.toString;
 
