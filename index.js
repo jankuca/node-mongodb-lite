9011606795de7082853f82569a42ae6753aa3b96
jankuca <jan@jankuca.com>
2014-04-12 04:37:08 +0200

    export Message constructors

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ var mongodb = {};
 
 mongodb.ReplicaSet = require('./src/ReplicaSet');
 mongodb.Server = require('./src/Server');
+mongodb.Connection = require('./src/Connection');
 
 mongodb.Collection = require('./src/Collection');
 mongodb.Command = require('./src/Command');
 mongodb.Database = require('./src/Database');
+
+mongodb.DeleteMessage = require('./src/DeleteMessage');
+mongodb.InsertMessage = require('./src/InsertMessage');
+mongodb.Message = require('./src/Message');
+mongodb.QueryMessage = require('./src/QueryMessage');
+mongodb.ReplyMessage = require('./src/ReplyMessage');
+mongodb.UpdateMessage = require('./src/UpdateMessage');
 
 mongodb.ObjectId = require('buffalo').ObjectId;
 mongodb.ObjectId.prototype.toJSON = mongodb.ObjectId.prototype.toString;
