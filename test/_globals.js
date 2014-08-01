d27be99afddd2e3e30ec2cb066fa14bc5af03099
jankuca <jan@jankuca.com>
2014-08-02 00:01:43 +0200

    improve test scripts, generate test coverage

--- test/_globals.js
+++ test/_globals.js
@@ -1,2 +1,17 @@
+var path = require('path');
+
 
 global.expect = require('expect.js');
+
+
+if (process.argv.indexOf('html-cov') !== -1) {
+  var blanket = require('blanket');
+  var repo_dirname = path.resolve(__dirname, '..');
+  blanket({
+    'pattern': new RegExp('^' + path.join(
+      repo_dirname,
+      'src',
+      '(?!' + path.join('test', 'mocks') + ')'
+    ))
+  });
+}
